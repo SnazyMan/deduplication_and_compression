6d053e88755e23aa90d5618c525410e7a7e44403
SnazyMan <sleepingzucchini@gmail.com>
2017-11-04 15:49:12 -0400

    Initial implementation of SHA256

--- dedup_main.c
+++ dedup_main.c
@@ -1,6 +1,26 @@
 #include "sha256.h"
+#include <string.h>
+#include <stdio.h>
 
+#define SHA_TEST 1
 
 int main()
 {
+	int ret = 0;
+	
+#ifdef SHA_TEST
+	int pass = 1;
+	unsigned char hash1[SHA256_BLOCK_SIZE] = {
+		0xba,0x78,0x16,0xbf,0x8f,0x01,0xcf,0xea,0x41,0x41,0x40,0xde,
+		0x5d,0xae,0x22,0x23,
+		0xb0,0x03,0x61,0xa3,0x96,0x17,0x7a,0x9c,0xb4,0x10,0xff,0x61,
+		0xf2,0x00,0x15,0xad
+	};
+	unsigned char *chunk1 = {"abc"};
+	unsigned char digest_out[SHA256_BLOCK_SIZE];
+	ret = sha256(chunk1, strlen(chunk1), digest_out);
+	pass = pass && !memcmp(hash1, digest_out, SHA256_BLOCK_SIZE);
+	printf("SHA-256 tests: %s\n", pass ? "SUCCEEDED" : "FAILED");		
+#endif	
+	return ret;
 }
--- sha256.h
+++ sha256.h
@@ -1,12 +1,29 @@
 #include <stdint.h>
+#include <stddef.h>
+
+/*
+ * Initial implementation referenced from:
+ * https://github.com/B-Con/crypto-algorithms/blob/master/sha256.c
+ * https://github.com/openssl/openssl/blob/master/crypto/sha/sha256.c
+ *
+ * Tested with test vectors from: https://www.di-mgt.com.au/sha_testvectors.html
+ *
+ * Naming convention mostly follows openssl hash implementations
+ */
+
+#define SHA256_BLOCK_SIZE 32 // 32 Byte digest
 
 struct sha256_ctx {
 	uint8_t data[64]; 
-	uint32_t data_len;
+	uint32_t datalen;
+	unsigned long long bitlen;
 	uint32_t nl;
 	uint32_t nh;
-	uint32_t state[8]; // h0-h7 : h0 || h1 ... || h7 = digest
+	uint32_t h[8]; // h0-h7 : h0 || h1 ... || h7 = digest
 };	
 	
-int sha256(char *input_chunk, int chunk_length, char *digest);
-int sha256_init(struct sha256_ctx *ctx);
+int sha256(unsigned char *input_chunk, int chunk_length, unsigned char *digest);
+void sha256_init(struct sha256_ctx *ctx);
+void sha256_update(struct sha256_ctx *ctx, const unsigned char *data, size_t len);
+void sha256_transform(struct sha256_ctx *ctx, const unsigned char *data);
+void sha256_final(struct sha256_ctx *ctx, unsigned char *hash);
