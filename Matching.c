d9e8aeaa0c7bc476279c4d74ade465c3d4ccfdc2
SnazyMan <sleepingzucchini@gmail.com>
2017-11-25 07:26:08 -0500

    Use c lib methods instead of ours to reduce chance of error

--- Matching.c
+++ Matching.c
@@ -1,4 +1,5 @@
 #include "Matching.h"
+#include <string.h>
 
 #pragma SDS data mem_attribute(historytable:PHYSICAL_CONTIGUOUS)
 void Matching(unsigned char digest[32], unsigned char historytable[1114112], int *LZWChunkNumber, char *deduplicate, int *index)
@@ -8,26 +9,22 @@ void Matching(unsigned char digest[32], unsigned char historytable[1114112], int
 
     // Check if valid bit is set 
     if (historytable[curHash + 33] & 1) {
-        // check for collision
+        // Compare the incoming digest with what exists in the historytable at its hash
     	int equal = 0;
-    	int j;
-    	for (j = 0; j < 32; j++) {
-    		if (digest[j] == historytable[curHash + j]) {
-    			equal++;
-    		}
-    	}
-    	// if there wasn't a collision, return the index
-    	if (equal == 32) {
-    		*index = (((int)historytable[curHash + j] << 8) | historytable[curHash + j + 1]) >> 1;
-    		*deduplicate = 1;
+	equal = memcmp(&historytable[curHash], digest, sizeof(unsigned char) * 32);
+	
+    	// if they were equal, return the index
+    	if (equal == 0) {
+    	    *index = (((int)historytable[curHash + 32] << 8) | historytable[curHash + 33]) >> 1;
+    	    *deduplicate = 1;
     	}
     	else { // linear probe to find empty entry or return match
-    		curHash = rehash(curHash, historytable, digest, deduplicate, index);
+    	    curHash = rehash(curHash, historytable, digest, deduplicate, index);
 		
-    		// If match wasnt found in rehash, insert the chunk
-    		if (*deduplicate == 0) {
-    			insert(curHash, historytable, digest, LZWChunkNumber);
-    		}
+    	    // If match wasnt found in rehash, insert the chunk
+    	    if (*deduplicate == 0) {
+	         insert(curHash, historytable, digest, LZWChunkNumber);
+	    }
     	}
     }
     else {
@@ -72,40 +69,35 @@ unsigned int rehash(unsigned int curHash, unsigned char historytable[1114112], u
     	rehash += 34;
     	rehash = rehash % 1114112;
     	if (historytable[rehash + 33] & 1) {
-    	    // Entry already exists here, check if chunk in question
-    		int equal = 0;
-    		int j;
-    		for (j = 0; j < 32; j++) {
-    			if (digest[j] == historytable[rehash + j]) {
-    				equal++;
-    			}
-    		}
-    		// if chunk was found, return the index
-    		if (equal == 32) {
-    			*index = (((int)historytable[rehash + j] << 8) | historytable[rehash + j + 1]) >> 1;
-    			*deduplicate = 1;
-    			return rehash;
-    		}
+    	    // Entry already exists here, check if incoming digest exists in the history table at this address
+    	    int equal = 0;
+	    equal = memcmp(&historytable[rehash], digest, sizeof(unsigned char) * 32);
+		
+	    // if chunk was found, return the index
+	    if (equal == 0) {
+	        *index = (((int)historytable[rehash + 32] << 8) | historytable[rehash + 33]) >> 1;
+		*deduplicate = 1;
+		return rehash;
+	    }
     	}
     	else {
-    		// Found free space
-    		*deduplicate = 0;
-    		return rehash;
+	    // Found free space
+    	    *deduplicate = 0;
+	    return rehash;
     	}
     }
 }
 
 void insert(unsigned int curHash, unsigned char historytable[1114112], unsigned char digest[32], int *LZWChunkNumber)
 {
-    int i;
-    for (i = 0; i < 32; i++) {
-        historytable[curHash + i] = digest[i];    	
-    }
+    // Copy the chunks digest into the history table
+    memcpy(&historytable[curHash], digest, sizeof(unsigned char) * 32);
 
     // add LZW chunk number and valid bit
-    historytable[curHash + i] = ((*LZWChunkNumber) & 0x0000FF00) >> 8;
-    historytable[curHash + i + 1] = ((*LZWChunkNumber) & 0x000000FF) | 1;
-    
+    historytable[curHash + 32] = ((*LZWChunkNumber) & 0x0000FF00) >> 8;
+    historytable[curHash + 33] = ((*LZWChunkNumber) & 0x000000FF) | 1;
+
+    // Increment the index so next chunk inserted will have correct index
     (*LZWChunkNumber)++;
 }
 
