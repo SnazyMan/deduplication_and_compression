c5fb73b572d0c6cbdf915a8899fd3eec2f9938c1
Renzhi Huang <32908764+RenzhiHuang@users.noreply.github.com>
2017-11-26 18:52:13 -0500

    CDC with lots of duliplicate
    prime 2
    window size 48
    minimum size 2KB
    start ahead 64 bytes

--- ContentDefinedChunk.c
+++ ContentDefinedChunk.c
@@ -2,23 +2,24 @@
 #include <math.h>
 #include "ContentDefinedChunk.h"
 
-#define WINDOW_SIZE (16)
-#define PRIME (23)
+#define WINDOW_SIZE (48)
+#define PRIME (2)
 #define CHUNK_AVERAGE_SIZE (12)
 #define MAX_CHUNK_SIZE 8192 // Bytes
-#define MIN_CHUNK_SIZE (512) // Bytes
+#define MIN_CHUNK_SIZE (2048) // Bytes
 #define FINGERPRINT_BITS (64)
 
-void ContentDefinedChunk(const unsigned char *Input, unsigned int *ChunkLength,
-			 unsigned int *ChunkNumber, int in_len)
+void ContentDefinedChunk(const unsigned char *Input, int *ChunkLength,
+			 int *ChunkNumber, int in_len)
 {
     long Modulus = (long)pow(2, FINGERPRINT_BITS);
-    long POW = ((long)pow(PRIME, WINDOW_SIZE)) % Modulus;
+    long POW = (long)(pow(PRIME, WINDOW_SIZE)) % Modulus;
     long rollhash = 0;
     unsigned int j = 0;
+    /*
     unsigned int chunklength = WINDOW_SIZE;
     
-    for (int i = (MIN_CHUNK_SIZE -1); i < (in_len - WINDOW_SIZE); i++) {
+    for (int i = 0; i < (in_len - WINDOW_SIZE); i++) {
         rollhash = (rollhash * PRIME + Input[i + WINDOW_SIZE] - Input[i] * POW) % Modulus;
 	
         chunklength++;
@@ -27,8 +28,27 @@ void ContentDefinedChunk(const unsigned char *Input, unsigned int *ChunkLength,
 	// End the chunk at max size, at content marker, or when out of data
         if (((chunklength >= MIN_CHUNK_SIZE) && (chunklength == MAX_CHUNK_SIZE || lowerbits == 0)) || i == (in_len - WINDOW_SIZE - 1)) {
             ChunkLength[j++] = chunklength;
+            printf("chunklength%d\n",chunklength);
             (*ChunkNumber)++;
             chunklength = 0;
+            rollhash=0;
         }
+    }
+     */
+    unsigned int chunklength = MIN_CHUNK_SIZE-1-64+WINDOW_SIZE;
+    for(int i =MIN_CHUNK_SIZE-1-64; i< (in_len-WINDOW_SIZE);i++){
+        
+        rollhash = (rollhash * PRIME + Input[i + WINDOW_SIZE] - Input[i] * POW) % Modulus;
+        chunklength++;
+        long lowerbits = rollhash & ((long)pow(2, CHUNK_AVERAGE_SIZE) - 1);
+        if (((chunklength >= MIN_CHUNK_SIZE) && (chunklength == MAX_CHUNK_SIZE || lowerbits == 0)) || i == (in_len - WINDOW_SIZE - 1)) {
+            ChunkLength[j++] = chunklength;
+            printf("chunklength%d\n",chunklength);
+            (*ChunkNumber)++;
+            chunklength = MIN_CHUNK_SIZE-1-64+WINDOW_SIZE;
+            rollhash=0;
+            i = i+MIN_CHUNK_SIZE-1-64+WINDOW_SIZE;
+        }
+
     }
 }
