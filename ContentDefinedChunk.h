981f8bada0444f99327a99c670604c672de5f5fe
Renzhi Huang <32908764+RenzhiHuang@users.noreply.github.com>
2017-11-26 19:04:07 -0500

    CDC.h

--- ContentDefinedChunk.h
+++ ContentDefinedChunk.h
@@ -1,15 +1,7 @@
-
 #ifndef ContentDefinedChunk_h
 #define ContentDefinedChunk_h
-#include "io.h"
 
-#define MAX_CHUNK_SIZE 8192 // Bytes
-#define MIN_CHUNK_SIZE 512 // Bytes
-
-#define WINDOW_SIZE 16
-
-int ContentDefinedChunk(const unsigned char Input[MAX_CHUNK_SIZE] , unsigned char Output[MAX_CHUNK_SIZE]);
-
-void fill_irp_table();
+void ContentDefinedChunk(const unsigned char * Input, int * ChunkLength, int *ChunkNumber,
+			 int in_len);
 
 #endif /* ContentDefinedChunk_h */
